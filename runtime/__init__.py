e6fa14b723f9fc30a99184162dd84c9023c06746
Colin Downie <91563252+ColinDownie@users.noreply.github.com>
2026-05-21 10:42:21 +0100

    Add runtime module initialization

---
+++ runtime/__init__.py
@@ -0,0 +1,8 @@
+"""Ross-OS Runtime Module
+
+FastAPI runtime shell for the NeuroSpatial Engine and Stability Mapper.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Colin Downie"
+__description__ = "Computational civilizational cybernetics engine runtime"
